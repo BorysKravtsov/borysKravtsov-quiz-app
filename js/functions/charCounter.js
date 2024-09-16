c2efdbdd8b9b1b2c8491a3982e8b2e7a5a288b50
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-16 14:00:05 +0200

    added counter

--- js/functions/charCounter.js
+++ js/functions/charCounter.js
@@ -1,5 +1,5 @@
-const questionChars = document.querySelector("questionChars");
-const answerChars = document.querySelector("answerChars");
+const questionChars = document.querySelector('[data-js="questionChars"]');
+const answerChars = document.querySelector('[data-js="answerChars"]');
 
 const questionArea = document.querySelector('[data-js="question"]');
 const answerArea = document.querySelector('[data-js="answer"]');
@@ -7,14 +7,12 @@ const answerArea = document.querySelector('[data-js="answer"]');
 const maxValueQuestion = questionArea.getAttribute("maxlength");
 const maxValueAnswer = answerArea.getAttribute("maxlength");
 
-console.log(maxValueQuestion);
-
-function charCounterAnswer(currentChar, element) {
-  const charLeft = Number.parseInt(maxValueAnswer) - currentChar;
-  element.textContent = charLeft;
-}
-
 answerArea.addEventListener("input", (event) => {
-  charCounterAnswer(event.target.value.length, answerChars);
-  console.log(maxValueQuestion);
+  answerChars.textContent =
+    maxValueAnswer - Number.parseInt(event.target.value.length);
+  console.log(answerArea.textContent);
+});
+questionArea.addEventListener("input", (event) => {
+  questionChars.textContent =
+    maxValueQuestion - Number.parseInt(event.target.value.length);
 });
